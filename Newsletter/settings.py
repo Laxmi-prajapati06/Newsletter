5ec963d49c6e3a8e85a4d089718480d665cc82fd
Laxmi-prajapati06 <laxmiprajapati006@gmail.com>
2025-12-20 06:32:59 +0530

    fix: remove untracked files and add .gitignore

--- Newsletter/settings.py
+++ Newsletter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@#zlob&xab(ip4c4$_k^w9md78w&y#rin%kt^i-k-ea$6l%=e_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Newsletter.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +125,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
